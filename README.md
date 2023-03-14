945b7e88adef91b468cea95d7fb965baa3118a26
mdcruz <marie.descruz@gmail.com>
2023-03-14 17:34:48 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# k6-browser-demo
+
+A simple project to demonstrate the latest version (v0.43.1) of k6 and k6 browser.
+
+## Prerequisites
+
+- [k6](https://k6.io/docs/get-started/installation/)
+
+## Running the test
+
+On your terminal, run the following command:
+
+`K6_BROWSER_ENABLED=true k6 run script.js`
---
+++ script.js
@@ -0,0 +1,50 @@
+import { chromium } from 'k6/experimental/browser'
+import { check } from 'k6'
+import http from 'k6/http'
+
+export const options = {
+  scenarios: {
+    browser: {
+      executor: 'per-vu-iterations',
+      exec: 'browser'
+    },
+    protocol: {
+      executor: 'constant-vus',
+      exec: 'protocol',
+      vus: 20,
+      duration: '30s',
+    },
+  }
+}
+
+export async function browser() {
+  const browser = chromium.launch({ headless: false });
+  const page = browser.newPage();
+
+  await page.goto('https://otel-demo.field-eng.grafana.net/');
+
+  const productCard = page.locator('(//div[@data-cy="product-card"])[1]');
+  await productCard.click();
+
+  const quantityOption = page.locator('[data-cy="product-quantity"]');
+  quantityOption.selectOption('3');
+
+  const addToCartBtn = page.locator('[data-cy="product-add-to-cart"]');
+  await addToCartBtn.click();
+  
+  check(page, {
+    'cart item name': page => page.locator('//p[text()="National Park Foundation Explorascope"]').isVisible() == true,
+    'cart item quantity': page => page.locator('//p[text()="3"]').isVisible() == true,
+  })
+
+  page.close();
+  browser.close();
+}
+
+export function protocol() {
+  const res = http.get('https://otel-demo.field-eng.grafana.net/');
+
+  check(res, {
+    'status is 200': res => res.status === 200,
+  });
+}
